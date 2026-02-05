84b031c092ca8612eeefd6448d9d3218f1f3d8ba
thirupathiaashrith6831 <thirupathiaashrith6831@gmail.com>
2026-02-05 21:28:06 +0530

    Add files via upload

---
+++ Pattern_5.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void main()
+{
+    int row,col;
+    printf("Enter the no. of Rows:\n");
+    scanf("%d",&row);
+    printf("Enter the no. of Columns:\n");
+    scanf("%d",&col);
+    for (int i=1;i<=row;i++)
+    {
+        for (int j=1;j<=col;j++)
+        {
+            if (i==1 || i==row || j==1 || j==col || i==j)
+            {
+                printf("*  ");
+            }
+            else
+            {
+              printf("   ");  
+            }
+        }
+        printf("\n");
+    }
+}
